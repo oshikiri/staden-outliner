13b23a76b0760d0e023fd9c1672e048070e767ec
Takamasa Oshikiri <mail@oshikiri.org>
2025-06-20 22:25:23 +0900

    Fix convertToMarkdownRecursive test

--- src/app/lib/exporter/incremental_exporter.test.ts
+++ src/app/lib/exporter/incremental_exporter.test.ts
@@ -7,7 +7,7 @@ import { convertToMarkdownRecursive } from "./incremental_exporter";
 describe("convertToMarkdownRecursive", () => {
   test("empty page", () => {
     const block = new Block([], 0, []);
-    expect(convertToMarkdownRecursive(block)).toBe("");
+    expect(convertToMarkdownRecursive(block)).toBe("- ");
   });
   test("page with 2 children", () => {
     const block = new Block([new Text("test")], 0, [
@@ -18,7 +18,7 @@ describe("convertToMarkdownRecursive", () => {
       new Block([], 1, []),
     ]);
     expect(convertToMarkdownRecursive(block)).toBe(
-      "\n- \n\t- 1-1\n\t- 1-2\n- ",
+      "- test\n\t- \n\t\t- 1-1\n\t\t- 1-2\n\t- ",
     );
   });
 });
