22b171452270d80cee2a1a171a12b6a565e15e8f
Takamasa Oshikiri <mail@oshikiri.org>
2025-06-24 23:45:32 +0900

    Add setup-vega

---
+++ scripts/setup-vega.sh
@@ -0,0 +1,5 @@
+mkdir public/
+
+wget https://cdn.jsdelivr.net/npm/vega@6 -O public/vega.js
+wget https://cdn.jsdelivr.net/npm/vega-lite@5 -O public/vega-lite.js
+wget https://cdn.jsdelivr.net/npm/vega-embed@6 -O public/vega-embed.js
