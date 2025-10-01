5c266185bcf3fddb415921533ade6c895c09c3fa
Takamasa Oshikiri <mail@oshikiri.org>
2025-10-01 21:34:26 +0900

    feat: redirect from the top page to the today's journal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import Link from "next/link";
+import type { JSX } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
-  return (
-    // RV: Provide a more descriptive link text for accessibility.
-    <Link href={"/pages/" + encodeURIComponent("メインページ")}>
-      メインページ
-    </Link>
-  );
+import { StadenDate } from "@/app/lib/date";
+
+export default function Home(): JSX.Element | null {
+  const router = useRouter();
+
+  useEffect(() => {
+    const title = new StadenDate().format();
+    router.replace(`/pages/${encodeURIComponent(title)}`);
+  }, [router]);
+
+  return null;
 }
