53ce40387d141fcba52c9db3f8d0de050c62d714
Takamasa Oshikiri <mail@oshikiri.org>
2025-11-09 01:11:29 +0900

    fix: streamline e2e global setup

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,11 +1,18 @@
-import { FullConfig, chromium } from "@playwright/test";
+import { FullConfig, expect, request } from "@playwright/test";
 
 async function globalSetup(config: FullConfig) {
   console.log(config.rootDir);
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto("http://localhost:3000/api/initialize");
-  await page.waitForTimeout(3000);
+  const requestContext = await request.newContext();
+
+  try {
+    const response = await requestContext.get(
+      "http://localhost:3000/api/initialize",
+    );
+
+    expect(response.status()).toBe(200);
+  } finally {
+    await requestContext.dispose();
+  }
 }
 
 export default globalSetup;
