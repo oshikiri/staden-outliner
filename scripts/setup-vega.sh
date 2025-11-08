748379db5f8b9c8d38d6aa4df30e8744870b9ec3
Takamasa Oshikiri <mail@oshikiri.org>
2025-11-09 00:45:03 +0900

    chore: Cache Vega bundles (#69)

--- scripts/setup-vega.sh
+++ scripts/setup-vega.sh
@@ -1,5 +1,8 @@
-mkdir --parents public/
+#!/usr/bin/env bash
+set -euo pipefail
 
-wget https://cdn.jsdelivr.net/npm/vega@6 -O public/vega.js
-wget https://cdn.jsdelivr.net/npm/vega-lite@5 -O public/vega-lite.js
-wget https://cdn.jsdelivr.net/npm/vega-embed@6 -O public/vega-embed.js
+mkdir -p public/
+
+curl -fsSL https://cdn.jsdelivr.net/npm/vega@6 -o public/vega.js
+curl -fsSL https://cdn.jsdelivr.net/npm/vega-lite@5 -o public/vega-lite.js
+curl -fsSL https://cdn.jsdelivr.net/npm/vega-embed@6 -o public/vega-embed.js
